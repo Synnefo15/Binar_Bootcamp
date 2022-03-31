a473ef032d49f36894fec6ca71d3d97daf7991b8
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2022-03-31 20:48:27 +0700

    opsi segitiga

--- mini-challange/oop-bangun_datar/script.js
+++ mini-challange/oop-bangun_datar/script.js
@@ -52,18 +52,32 @@ class Segitiga extends BangunDatar {
 		super();
 		this.alas = alas;
 		this.tinggi = tinggi;
-        this.miring = miring
+		this.miring = miring;
 	}
 	luas() {
 		const Luas = (1 / 2) * this.alas * this.tinggi;
 		console.log(`Luas Segitiga = ${Luas} `);
 	}
-    keliling(){
-        
-        const keliling = this.alas + (this.miring*2) // % segi3 samasisi 
-		console.log(`Keliling Segitiga = ${keliling}`);
-
-    }
+	keliling(jenis) {
+        let keliling = null
+		switch (jenis) {
+			case 'sama_kaki':
+				keliling = this.alas + this.miring * 2; // % segi3 samakaki
+				console.log(`Keliling Segitiga sama kaki = ${keliling}`);
+				break;
+            case 'sama_sisi':
+                keliling = this.alas*3
+				console.log(`Keliling Segitiga sama sisi = ${keliling}`);
+                break
+            case 'siku_siku':
+                keliling = this.alas + this.tinggi + this.miring
+				console.log(`Keliling Segitiga siku-siku = ${keliling}`);
+                break
+			default:
+                console.log(`Pilihan tidak tersedia `);
+				break;
+		}
+	}
 }
 
 const persegi1 = new Persegi(5);
@@ -76,7 +90,9 @@ console.log(persegiPanjang1);
 persegiPanjang1.luas();
 persegiPanjang1.keliling();
 
-const segitiga1 = new Segitiga(6,4,5)
+const segitiga1 = new Segitiga(6, 4, 5);
 console.log(segitiga1);
-segitiga1.luas()
-segitiga1.keliling()
+segitiga1.luas();
+segitiga1.keliling('sama_kaki');
+segitiga1.keliling('sama_sisi')
+segitiga1.keliling('siku_siku')
